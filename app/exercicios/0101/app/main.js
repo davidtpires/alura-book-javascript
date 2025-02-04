6e233b2c7187a839559ea68c5daa84cc8acc3d4f
David T. <davidt.info@gmail.com>
2025-02-04 14:42:29 -0300

    exercicio 0102

--- app/exercicios/0101/app/main.js
+++ app/exercicios/0101/app/main.js
@@ -1,10 +1,25 @@
 let livros = []
 const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
 getBuscarLivrosDaAPI()
+const elementoParaInserirLivros = document.getElementById('livros')
 
 async function getBuscarLivrosDaAPI() {
     const res = await fetch(endpointDaAPI)
     livros = await res.json()
     console.table(livros)
-    // exibirOsLivrosNaTela(livros)
+    exibirOsLivrosNaTela(livros)
+}
+
+function exibirOsLivrosNaTela(listaDeLivros) {
+    listaDeLivros.forEach(livro => {
+        elementoParaInserirLivros.innerHTML += `
+            <div>
+                <img src="${livro.imagem}" alt="${livro.alt}" />
+                <h2>${livro.titulo}</h2>
+                <p>${livro.autor}</p>
+                <p>R$${livro.preco}</p>
+                <span>${livro.categoria}</span>
+            </div>
+        `
+    });
 }
