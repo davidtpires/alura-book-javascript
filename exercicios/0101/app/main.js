33b528dce7bf7a4938820013a86b368e1d4cb477
David T. <davidt.info@gmail.com>
2025-02-05 16:22:52 -0300

    Como funciona o filter

--- exercicios/0101/app/main.js
+++ exercicios/0101/app/main.js
@@ -7,7 +7,17 @@ async function getBuscarLivrosDaAPI() {
     const res = await fetch(endpointDaAPI)
     livros = await res.json()
     console.table(livros)
-    exibirOsLivrosNaTela(livros)
+    let livrosComDesconto = aplicarDesconto(livros)
+    exibirOsLivrosNaTela(livrosComDesconto)
+}
+
+function aplicarDesconto(livro) {
+    const desconto = 0.2
+    livrosComDesconto = livros.map(livro => {
+        return {...livro, preco: livro.preco - (livro.preco * desconto)}
+    })
+
+    return livrosComDesconto
 }
 
 function exibirOsLivrosNaTela(listaDeLivros) {
@@ -17,7 +27,7 @@ function exibirOsLivrosNaTela(listaDeLivros) {
                 <img src="${livro.imagem}" alt="${livro.alt}" />
                 <h2>${livro.titulo}</h2>
                 <p>${livro.autor}</p>
-                <p>R$${livro.preco}</p>
+                <p>R$${livro.preco.toFixed(2)}</p>
                 <span>${livro.categoria}</span>
             </div>
         `
