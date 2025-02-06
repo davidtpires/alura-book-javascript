00e3ec756e9b0932bb1ec52e813c87b60196257c
David T. <davidt.info@gmail.com>
2025-02-06 10:19:48 -0300

    Filtrando livros disponíveis

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -5,7 +5,8 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
-    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
+    console.log(categoria)
+    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria)
     // console.table(livrosFiltrados)
     exibirOsLivrosNaTela(livrosFiltrados);
 }
